c70b6c19e477b720eebbee8e8aa797b698d3da68
7yao_Gao <597449675@qq.com>
2017-04-15 18:42:00 +0800

    add some file

---
+++ qiyao/check/static/js/Tree.js
@@ -0,0 +1,64 @@
+﻿// JavaScript Document
+
+$(function(){
+	$('#Tree .com_name label').click(
+		function(){
+			$('#Tree_view').toggleClass("open");
+			$(this).toggleClass("Enlarge");
+			
+		}
+	)
+	
+	$('#Tree span label').click(
+		function(){			
+			$(this).toggleClass("Enlarge");
+			$(this).parent().siblings(".Tree_view_sub").toggleClass("open");
+		}
+	)
+	
+	$("#Tree span").hover(
+		function () {
+			$(this).toggleClass("over");
+		},
+		function () {
+			$(this).toggleClass("over");
+		}
+	); 
+	
+	
+	$('.perS .d_title ul li').click(
+		function(){
+			$(this).siblings("li").removeClass("li_sel");
+			$(this).addClass("li_sel");
+		}
+	)
+	
+	/*$('#d_list_view .group span').click(
+		function(){
+			$(this).parent().toggleClass("sel_d");
+			$('#d_list_view .group').removeClass("show");
+			$(this).parent().addClass("show");
+			
+			$(this).parent().next(".list_view_sub").toggleClass("open");
+		}
+	)*/
+	
+	$('#d_list_view li').click(
+		function(){
+			$(this).toggleClass("seled"); 
+		}
+	);
+	
+	
+	$("#expression .index").toggle(
+		function () {
+			$("#expression").hide();
+			$(this).css("background-position","2px -16px");
+		},
+		function () {
+			$("#per_list").show();
+			$(this).css("background-position","3px 4px");
+		}
+	)
+	
+})
